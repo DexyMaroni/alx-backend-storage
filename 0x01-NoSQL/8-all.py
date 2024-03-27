e26a3f3cc0ee66975371d45f1fa9f46f71a13465
DexyMaroni <divineinyang7.com>
2024-03-26 17:27:42 -0700

    Mongodb_2

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A function that lists all documents in a collection"""
+
+def list_all(mongo_collection):
+    """
+    Returns an empty list if no document in the collection
+    """
+    return mongo_collection.find()
